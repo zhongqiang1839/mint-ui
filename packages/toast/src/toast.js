8f61072df072449a27e822536edb9127c4c09853
冯忠强 <zhongqiang1839@163.com>
2018-03-28 14:17:28 +0800

    Update toast.js
    获得的 实例 instance.$el 为 空的  <div></div>

--- packages/toast/src/toast.js
+++ packages/toast/src/toast.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
+import toastVue from './toast.vue';
 
-const ToastConstructor = Vue.extend(require('./toast.vue'));
+const ToastConstructor = Vue.extend(toastVue);
 let toastPool = [];
 
 let getAnInstance = () => {
